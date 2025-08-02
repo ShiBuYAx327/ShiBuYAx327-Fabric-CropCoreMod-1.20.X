21b9670803beb5955515bca391f67128a15f7365
ShiBuYAx327 <Nattapat.mal@ku.th>
2025-08-03 04:02:23 +0700

    added important comment

---
+++ src/main/java/net/shibuya/cropcoremod/CropcoreModClient.java
@@ -0,0 +1,10 @@
+package net.shibuya.cropcoremod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CropcoreModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/shibuya/cropcoremod/Cropcoremod.java
@@ -0,0 +1,17 @@
+package net.shibuya.cropcoremod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// eieieieieieieiei
+public class Cropcoremod implements ModInitializer {
+	public static final String MOD_ID = "cropcoremod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
